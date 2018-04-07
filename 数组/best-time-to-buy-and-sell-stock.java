bcd96fc726196903bcaba3bc445db7b460f9adf1
MrQuJL <909301740@qq.com>
2018-04-07 09:21:06 +0800

    update the thinking of buy one and sell one goods

--- 数组/best-time-to-buy-and-sell-stock.java
+++ 数组/best-time-to-buy-and-sell-stock.java
@@ -3,12 +3,13 @@
  * 题目描述：Say you have an array for which the i th element is the price of a given stock on day i.
  * If you were only permitted to complete at most one transaction
  * (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit.
- * 题意：做一次买入卖出的最大收益
+ * 题意：做一次买入卖出的最大收益（只准扫一次数组，先买后卖）
  */
 public class Solution {
     /**
      * 思路：
      * 扫描一次找出最小值和最大值的差值
+	 * 其实就是求一个连续递增序列的首尾差值
      */
     public int maxProfit(int[] prices) {
         // 合法性判断
